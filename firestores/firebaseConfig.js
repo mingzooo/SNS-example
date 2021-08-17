56661c6d77691827b4e64d1c93cacc9c4ec3af0c
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-08-17 23:03:38 +0900

    feat : firebase

---
+++ firestores/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNU-v3hvSw1-nRdWPy5CtLv67DFyvhNR8",
+    authDomain: "sullivan-sns-51c46.firebaseapp.com",
+    projectId: "sullivan-sns-51c46",
+    storageBucket: "sullivan-sns-51c46.appspot.com",
+    messagingSenderId: "659504481123",
+    appId: "1:659504481123:web:4cfe35b95601f7787e8956"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
